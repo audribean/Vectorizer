05ce5e3e56a7ebc56c779031f5c38f6bf0fb10ca
dol <dol@Audris-MacBook-Air-1820.local>
2026-03-14 17:37:46 +0100

    fixing port issue

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ def process():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    # app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
